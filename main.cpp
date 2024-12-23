d7e4299a933360af6efe4f1e0addd07772190835
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-12-23 19:49:00 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,11 +19,30 @@ public:
         cout << "Time Period = ";
         cin >> TimePeriod;
     }
-   
+    float emi()
+    {
+        requirements();
+        
+        // EMI calaculator
+        float temp = pow((1 + Intrest), TimePeriod);
+        float EMI = ((Principal * Intrest * temp) / (temp - 1));
+        return EMI;
+    }
+    float pre_emi()
+    {
+        // Pre Emi Calculation
+        float pre_emi_intrest = Principal * Intrest;
+        int total_pre_emi = pre_emi_intrest * 12;
+        return total_pre_emi;
+    }
 };
 int main()
 {
     Solution s;
     s.requirements();
+    int emi = s.emi();
+    int pre_emi = s.pre_emi();
+    cout << emi << endl;
+    cout << pre_emi << endl;
     return 0;
 }
