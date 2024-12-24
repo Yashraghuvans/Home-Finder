dc0bbc4aff30b3079e30e77e4bc86f92de320ee3
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-12-24 15:01:33 +0530

    calculater

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 using namespace std;
 
-class data{
-    public:
+class data
+{
+public:
     // All The Requirements To Calculate EMI
     long long int Principal;
     float Intrest;
     int TimePeriod;
 };
 
-class Requirements:public data
+class Requirements : public data
 {
 public:
     void requirements()
@@ -24,11 +25,31 @@ public:
         cin >> TimePeriod;
     }
 };
+class calculate:public data
+{
+public:
+    float emi()
+    {
+
+        // EMI calaculator
+        float temp = pow((1 + Intrest), TimePeriod);
+        float EMI = ((Principal * Intrest * temp) / (temp - 1));
+        return EMI;
+    }
+    float pre_emi()
+    {
+        // Pre Emi Calculation
+        float pre_emi_intrest = Principal * Intrest;
+        int total_pre_emi = pre_emi_intrest * 12;
+        long long int total_repay_amount = Principal * total_pre_emi;
+        return total_pre_emi;
+    }
+};
 
 int main()
 {
     Requirements req;
-    
+
     req.requirements();
 
     return 0;
