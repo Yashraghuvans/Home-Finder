69172e4a42b58a8b2da42b9c9c10501dc02e8dc7
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-12-24 14:58:18 +0530

    making inherant classes

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-class Solution
+class Requirements
 {
 public:
     // All The Requirements To Calculate EMI
@@ -19,29 +19,13 @@ public:
         cout << "Time Period = ";
         cin >> TimePeriod;
     }
-    float emi()
-    { 
-        // EMI calaculator
-        float temp = pow((1 + Intrest), TimePeriod);
-        float EMI = ((Principal * Intrest * temp) / (temp - 1));
-        return EMI;
-    }
-    float pre_emi()
-    {
-        // Pre Emi Calculation
-        float pre_emi_intrest = Principal * Intrest;
-        int total_pre_emi = pre_emi_intrest * 12;
-        long long int total_repay_amount = Principal * total_pre_emi;
-        return total_pre_emi;
-    }
 };
+
 int main()
 {
-    Solution s;
-    s.requirements();
-    int emi = s.emi();
-    int pre_emi = s.pre_emi();
-    cout << emi << endl;
-    cout << pre_emi << endl;
+    Requirements req;
+    
+    req.requirements();
+
     return 0;
 }
